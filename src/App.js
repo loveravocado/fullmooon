dc88d0982d4d1b2196fd20db54d5dd4ef01db836
loveravocado <makaron-ayako@docomo.ne.jp>
2023-11-19 15:50:41 +0900

    inputfullmoon

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import background from "./img/Fullmooon_background01.png"
+import background from "./img/Fullmooon_background01.png";
+import fullmoon from "./img/Fullmooon_fullmoon.png";
 import './App.css';
 
 export default function App(){
   return(
     <>
+    <div className="main">
       <div className="background" style={{ backgroundImage: `url(${background})` }}></div>
+      <img className="fullmoon" src={ fullmoon } />
+    </div>
       
     </>
   )
