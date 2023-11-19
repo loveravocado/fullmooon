615bd19454cb4cfbc67524349b81e54c653152fc
loveravocado <makaron-ayako@docomo.ne.jp>
2023-11-19 15:04:05 +0900

    background

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import background from "./img/Fullmooon_background01.png"
+import './App.css';
+
+export default function App(){
+  return(
+    <>
+      <div className="background" style={{ backgroundImage: `url(${background})` }}></div>
+      
+    </>
+  )
+}
